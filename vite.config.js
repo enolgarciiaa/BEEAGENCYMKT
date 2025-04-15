0330b2b918d14ef8dcd633e38fe6dbb6f7028d3b
Enol García Heres <Enolgarciiaa@gmail.com>
2025-04-16 00:22:06 +0200

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'; // Asegúrate de importar esto
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // ⚠️ importante si estás en la raíz del dominio
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
   build: {
